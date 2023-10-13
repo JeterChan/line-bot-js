4f91a7de51327094cca223b73c5f3ca37e3c9ac0
Jeter Chan <l6756071@gmail.com>
2023-10-13 16:53:27 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const https = require("https");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
+
 app.post("/webhook", function (req, res) {
     res.send("HTTP POST request sent to the webhook URL!");
     // If the user sends a message to your bot, send a reply message
@@ -14,7 +31,7 @@ app.post("/webhook", function (req, res) {
           },
           {
             type: "text",
-            text: "Good!",
+            text: "May I help you?",
           },
         ],
       });
@@ -57,3 +74,7 @@ app.post("/webhook", function (req, res) {
       request.end();
     }
   });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
